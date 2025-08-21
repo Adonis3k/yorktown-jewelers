e43c7d93eb8255db06486a60e9bfbf3cee588e2f
Adonis3k <adonisdmadera@gmail.com>
2025-08-21 18:36:12 -0400

    www.yorktown-jewelers.com

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/yorktown-jewelers/',
+  base: '/', // ✅ Use root base for custom domains
 })
